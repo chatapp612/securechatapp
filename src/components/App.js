28b7893a78b3e92eef6c4cf3be94167bde2620bc
Aditi <chatapp612@gmail.com>
2024-11-19 16:19:16 +0530

    sessionkeyencryption1

--- src/components/App.js
+++ src/components/App.js
@@ -185,7 +185,7 @@ const App = () => {
                 console.log("Padded Envelope:", paddedmessage);
     
                 // Encrypt the session key with the recipient's public key
-                const encryptedSessionKey = encryptSessionKey(sessionKey, recipientPublicKeyHex);
+                const encryptedSessionKey = await encryptSessionKey(sessionKey, recipientPublicKeyHex);
                 console.log("encrypted session key", encryptSessionKey);
                 // Store the encrypted session key on-chain
                 await contract.methods.storeSessionKey(recipient, encryptedSessionKey).send({ from: account });
