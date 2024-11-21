ed819a2c876ff820afa2707f9caf938452625444
Aditi <chatapp612@gmail.com>
2024-11-21 14:58:34 +0530

    sodium7

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,21 @@
 module.exports = {
     webpack: (config) => {
+      // Adding Babel loader for .js files
       config.module.rules.push({
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ['@babel/preset-env', '@babel/preset-react'], // Ensure React preset is included
+            plugins: [
+              '@babel/plugin-proposal-optional-chaining', // Enable optional chaining support
+              '@babel/plugin-proposal-nullish-coalescing-operator' // Enable nullish coalescing operator support
+            ]
           },
         },
       });
+  
       return config;
     },
   };
