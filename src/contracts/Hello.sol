715d38cf8497ae2bc7393da22abed5e509833154
Aditi-Wagh <106764459+Aditi-Wagh@users.noreply.github.com>
2024-12-14 16:25:08 +0530

    solving errors

---
+++ src/contracts/Hello.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+
+contract Hello {
+    string private storedString;
+
+    event StringSet(string newString);
+
+    function setString(string memory newString) public {
+        storedString = newString;
+        emit StringSet(newString);
+    }
+
+    function print() public view returns (string memory) {
+        return storedString;
+    }
+}
