4326829b2c3f68e468df74d01258f84ccb8e4450
Aditi-Wagh <106764459+Aditi-Wagh@users.noreply.github.com>
2024-12-16 21:08:57 +0530

    Updates

---
+++ precache-manifest.6d5099038770382ee04441b3e503938a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "abf9a1cc522c4e8bb19e",
+    "url": "/securechatapp/static/css/main.9433f48a.chunk.css"
+  },
+  {
+    "revision": "abf9a1cc522c4e8bb19e",
+    "url": "/securechatapp/static/js/main.abf9a1cc.chunk.js"
+  },
+  {
+    "revision": "147d9970cfa90e8e3feb",
+    "url": "/securechatapp/static/js/1.147d9970.chunk.js"
+  },
+  {
+    "revision": "b5e71cf304925104fb56",
+    "url": "/securechatapp/static/js/runtime~main.b5e71cf3.js"
+  },
+  {
+    "revision": "277b79b5b06a71432cb72541d4df5cd9",
+    "url": "/securechatapp/index.html"
+  }
+];
